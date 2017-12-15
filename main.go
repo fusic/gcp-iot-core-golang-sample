e18ebbd9738b396150e423ef544c123899bf278c
Takeshi Watanabe <take-cheeze@users.noreply.github.com>
2017-12-15 10:10:19 +0900

    Add comments.

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+// GCP settings passed by CLI parameters
 var (
 	project     = flag.String("project_id", "", "Project ID")
 	registry    = flag.String("registry_id", "", "Registry ID")
@@ -21,21 +22,24 @@ var (
 	private_key = flag.String("private_key_file", "", "Path to Private Key File")
 )
 
+// MQTT parameters
 const (
-	topicType = "events" // or "state"
-	qos = 1
+	topicType = "events"   // or "state"
+	qos = 1                // QoS 2 isn't supported in GCP
 	retain = false
-	username = "unused"
-	region = "us-central1"
+	username = "unused"    // always this value in GCP
+	region = "us-central1" // GCP region
 )
 
 func main() {
 	flag.Parse()
 
+	// generate MQTT client
 	client_id := fmt.Sprintf(
 		"projects/%s/locations/%s/registries/%s/devices/%s",
 		*project, region, *registry, *device)
 
+	// load private key
 	keyData, err := ioutil.ReadFile(*private_key)
 	if err != nil { panic(err) }
 
@@ -53,6 +57,7 @@ func main() {
 	}
 	if err != nil { panic(err) }
 
+	// generate JWT as the MQTT password
 	t := time.Now()
 	token := jwt.NewWithClaims(jwt.GetSigningMethod(*algorithm), &jwt.StandardClaims{
 		IssuedAt: t.Unix(),
@@ -62,6 +67,7 @@ func main() {
 	pass, err := token.SignedString(key)
 	if err != nil { panic(err) }
 
+	// configure MQTT client
 	opts := mqtt.NewClientOptions().
 		AddBroker(fmt.Sprintf("ssl://mqtt.googleapis.com:8883")).
 		SetClientID(client_id).
@@ -72,14 +78,17 @@ func main() {
 
 	conn := mqtt.NewClient(opts)
 
+	// connect to GCP Cloud IoT Core
 	fmt.Println("Connecting...")
 	tok := conn.Connect()
 	if err := tok.Error(); err != nil { panic(err) }
 	if !tok.WaitTimeout(time.Second * 5) { panic(fmt.Errorf("Connection Timeout")) }
 	if err := tok.Error(); err != nil { panic(err) }
 
+	// generate topic
 	topic := fmt.Sprintf("/devices/%s/%s", device, topicType)
 
+	// publish message 5 times
 	for i := 0; i < 5; i++ {
 		str := fmt.Sprintf("Message %d", i)
 		fmt.Printf("Publishing: '%s'\n", str)
@@ -89,6 +98,7 @@ func main() {
 
 	// need mqtt reconnect each 20 minutes for long use
 
+	// disconnect
 	fmt.Println("Disconnecting...")
 	conn.Disconnect(1000)
 }
